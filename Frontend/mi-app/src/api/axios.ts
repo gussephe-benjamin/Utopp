14ea466b6d5fd6834e0b21acb0ac40b9a8b3bcca
Gussephe Benjamin <gussephe.ayra@utec.edu.pe>
2026-03-05 01:02:18 -0500

    arreglo dockerfile despliegue

--- Frontend/mi-app/src/api/axios.ts
+++ Frontend/mi-app/src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+const baseURL = import.meta.env.VITE_API_URL 
+
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
